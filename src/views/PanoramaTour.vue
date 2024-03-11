d4deccc1d62e7218328d24687e960eba514d588b
yy6768 <1258720926@qq.com>
2024-03-11 21:49:04 +0800

    Init three.js & init 3d map

--- src/views/PanoramaTour.vue
+++ src/views/PanoramaTour.vue
@@ -1,11 +1,21 @@
 <template>
     <div class="about">
       <h1>This is 全景导览，</h1>
+      <PanoramaMap></PanoramaMap>
     </div>
-  </template>
+</template>
   
+<script lang="ts">
+import PanoramaMap from '../components/PanoramaMap.vue'
+export default {
+  name:"PanoramaTour",
+  components:{
+    PanoramaMap
+  }
+}
+</script>
   
-  <style>
+<style>
   
-  </style>
+</style>
   
